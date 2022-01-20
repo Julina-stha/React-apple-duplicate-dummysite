cfe8f2ba34b9459b60fa39eb3ce579ded4289df6
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-01-20 16:17:19 +0000

    Second component that includes the second section of main page content

---
+++ src/SecondContent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SecondContent.css';
+import './FirstMainContent.css';
+
+
+export default function SecondContent() {
+  return (
+    <div className='second-container'>
+      <a href="https://www.apple.com/uk/iphone-13/" className='h3andh4' target="_blank" rel="noreferrer">
+        <div className='secondMainContentHeader'>
+          <h3 className='iPhone13'>iPhone 13</h3>
+          <h4 className='superpower'>Your new superpower.</h4>
+          <div className='sub-text'>
+            <a href='https://www.apple.com/uk/iphone-13/' id='sub-text2' target="_blank" rel="noreferrer">Learn more</a>
+            <span id='divider'>{'>'}</span>
+            <a href='https://www.apple.com/uk/shop/buy-iphone/iphone-13' id='sub-text3' target="_blank" rel="noreferrer">Buy</a>
+            <span id='divider'>{'>'}</span>
+          </div>
+        </div>
+        <img src="https://www.apple.com/v/home/aj/images/heroes/iphone-13/hero_iphone_13__f194u1rdooeq_large.jpg" alt="iphone13"></img>
+      </a>
+    </div>
+  )
+}
