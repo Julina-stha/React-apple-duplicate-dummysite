4e5ad038e8d3e08f537e2468c6a62d33da5b61bd
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-01-20 11:37:26 +0000

    Added new component to handle the main content

---
+++ src/FirstMainContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FirstMainContent() {
+  return (
+    <div className="container">
+      <img src="https://www.apple.com/v/home/aj/images/heroes/iphone-13-pro/hero_iphone_13pro__dhnsegysa42u_large.jpg"></img>
+    </div>
+  )
+}
