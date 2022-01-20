9933c64d617ad8e4dd31c41702fab83403471e8e
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-01-20 16:15:20 +0000

    Added two new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Header';
+import FirstMainContent from './FirstMainContent';
+import SecondContent from './SecondContent';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FirstMainContent />
+      <SecondContent />
     </div>
   );
 }
