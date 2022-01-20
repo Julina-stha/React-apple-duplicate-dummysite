61e1561127a33762ad3d8d31832be92e5ec4b793
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-01-20 18:23:43 +0000

    Added new component to include the third section of the page

---
+++ src/ThirdContent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './FirstMainContent.css';
+import './ThirdContent.css';
+
+
+export default function ThirdContent() {
+  return (
+    <div className='third-container'>
+      <a href="https://www.apple.com/uk/apple-watch-series-7/" className='thirdContentLink'target="_blank" rel="noreferrer">
+        <div className='thirdContentHeader'>
+          <div className='new'>New</div>
+          <h5 className='watch7'><i class="fab fa-apple"></i>WATCH</h5>
+          <div className='series7'>SERIES 7</div>
+          <p>Introducing our largest display yet.</p>
+          <div className='sub-text'>
+            <a href='https://www.apple.com/uk/iphone-13/' id='sub-text2' target="_blank" rel="noreferrer">Learn more</a>
+            <span id='divider'>{'>'}</span>
+            <a href='https://www.apple.com/uk/shop/buy-iphone/iphone-13' id='sub-text3' target="_blank" rel="noreferrer">Buy</a>
+            <span id='divider'>{'>'}</span>
+          </div>
+        </div>
+        <img src="https://www.apple.com/v/home/aj/images/heroes/watch-series-7/hero_watch__f05go333pz2i_large.jpg" alt="watch-series7"></img>
+      </a>
+    </div>
+  )
+}
