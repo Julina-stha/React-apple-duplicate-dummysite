ef30b8a2584b680495f5e4cf7af24c46d84a1b89
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-01-24 16:42:56 +0000

    Added new component and CSS file to handle the last section of the main content page

---
+++ src/LastContent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './LastContent.css';
+
+
+export default function LastContent() {
+  return(
+    <section className='last-container'>
+      <div className='last-paragraph'>
+        <ul>
+          <li>1. £9.99/month after trial. Offer valid for three months after eligible device activation. One subscription per Family Sharing group. Plan automatically renews until cancelled. Restrictions and other terms apply.</li>
+          <li>A subscription is required for Apple Fitness+.</li>
+          <li>Apple Fitness+ requires iOS 14.3 or later, iPadOS 14.3 or later, watchOS 7.2 or later, and tvOS 14.3 or later.</li>
+          <li>To get the newest features, use Apple Fitness+ with Apple Watch Series 3 or later with watchOS 8 or later, paired with iPhone 6s or later with iOS 15 or later; iPad with iPadOS 15 or later; and Apple TV 4K or Apple TV HD with tvOS 15 or later.</li>
+          <li>Apple TV+ is £4.99/month after free trial. One subscription per Family Sharing group. Offer valid for three months after eligible device activation. Plan automatically renews until cancelled. Restrictions and other terms apply.</li>
+        </ul>
+      </div>
+      <hr></hr>
+      <div className='col-2'>
+        <ul className='lastContent-links'> 
+          <li>Shop and Learn</li>
+          <li>Services</li>
+          <li>Apple Store</li>
+          <li>For Business</li>
+          <li>Apple Values</li>
+          <li>Shop and Learn</li>
+          <li>Services</li>
+          <li>Apple Store</li>
+          <li>For Business</li>
+          <li>Apple Values</li>
+        </ul>
+      </div>
+    </section>
+  )
+}
