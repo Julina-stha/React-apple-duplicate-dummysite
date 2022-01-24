ec12e2d9e3a3de4ac9d5293ad0001d91e88f8a49
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-01-24 12:13:04 +0000

    amended an error

--- src/Header.js
+++ src/Header.js
@@ -13,12 +13,12 @@ export default function Header() {
         <li><a href="https://www.apple.com/uk/iphone/" target="_blank" rel="noreferrer" id="header-link">iPhone</a></li>
         <li><a href="https://www.apple.com/uk/watch/" target="_blank" rel="noreferrer" id="header-link">Watch</a></li>
         <li><a href="https://www.apple.com/uk/airpods/" target="_blank" rel="noreferrer" id="header-link">AirPods</a></li>
-        <li><a href="https://www.apple.com/uk/tv-home/" target="_blank" rel="noreferrer" id="header-link">TV & Home</a></li>
+        <li><a href="https://www.apple.com/uk/tv-home/" target="_blank" rel="noreferrer" id="header-link">TV {'&'} Home</a></li>
         <li><a href="https://www.apple.com/uk/services/" target="_blank" rel="noreferrer" id="header-link">Only on Apple</a></li>
         <li><a href="https://www.apple.com/uk/shop/accessories/all" target="_blank" rel="noreferrer" id="header-link">Accessories</a></li>
         <li><a href="https://support.apple.com/en-gb" target="_blank" rel="noreferrer" id="header-link">Support</a></li>
-        <li className="search-icon"><img src="https://img.icons8.com/ios/25/ffffff/search--v1.png"/></li>
-        <li><a href="https://www.apple.com/uk/store" target="_blank" rel="noreferrer" id="header-link"><img src="https://img.icons8.com/ios/15/ffffff/shopping-bag--v1.png"/></a></li>
+        <li className="search-icon"><img src="https://img.icons8.com/ios/25/ffffff/search--v1.png" alt='searchIcon' /></li>
+        <li><a href="https://www.apple.com/uk/store" target="_blank" rel="noreferrer" id="header-link"><img src="https://img.icons8.com/ios/15/ffffff/shopping-bag--v1.png" alt='shoppingCart' /></a></li>
       </ul>
     </div>
 
