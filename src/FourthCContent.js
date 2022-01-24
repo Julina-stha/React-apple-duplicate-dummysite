40c3d4a553be39355c380c8ea09b8f8b00c715ac
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-01-24 15:48:01 +0000

    Made styling improvements to the component

--- src/FourthCContent.js
+++ src/FourthCContent.js
@@ -10,25 +10,29 @@ export default function FourthCContent() {
           <div className='iPad'>iPad 
             <span className="coloredFont">  mini</span>
           </div>
-          <div id='megapower'>Megapower. Mini size. </div>
-          <div id='blue-links'>
+          <div className='megapower'>Megapower. Mini size. </div>
+          <div id='iPadblue-links'>
             <a href='https://www.apple.com/uk/ipad-mini/' id='blue-text2' target="_blank" rel="noreferrer">Learn more</a>
             <span id='divider'>{'>'}</span>
             <a href='https://www.apple.com/uk/shop/buy-ipad/ipad-mini' id='blue-text3' target="_blank" rel="noreferrer">Buy</a>
             <span id='divider'>{'>'}</span>
           </div>
         </div>
-        <img src="https://www.apple.com/v/home/aj/images/promos/ipad-mini/promo_ipad_mini__spq4zjcuuaie_large.jpg" alt="iPadMini"></img>
+        <img src="https://www.apple.com/v/home/aj/images/promos/ipad-mini/promo_ipad_mini__spq4zjcuuaie_large.jpg" alt="iPadMini" className='iPadMiniImage'></img>
       </a>
-      <a href="https://www.apple.com/uk/homepod-mini/" className='tv+Content' target="_blank" rel="noreferrer">
-        <div className='tv+ContentHeader'>
-          <div className='tv+'><i class="fab fa-apple"></i>tv+</div>
-          <div id='gift'>THE TRAGEDY OF </div>
-          <div id='gift' className='timeToRun'>MAcBETH</div>
-          <div id='gift' className='timeToRun'>Directed by Joel Coen</div>
-          <div id='blue-links'>
-            <a href='https://www.apple.com/uk/homepod-mini/' id='blue-text2' target="_blank" rel="noreferrer">Stream now</a>
-            <span id='divider'>{'>'}</span>
+      <a href="https://tv.apple.com/gb/movie/umc.cmc.4wpfk1xmi22h3zyv4a10lj1tw?ign-itscg=10000&ign-itsct=atv-apl_hp-pmo_wch-Macbeth-220113/" className='tv+Content' target="_blank" rel="noreferrer">
+        <div className='tvPlusContentHeader'>
+          <div className='tvPlus'><i class="fab fa-apple"></i>tv+</div>
+          <div className='title-small'>THE <img src="https://img.icons8.com/small/12/ffffff/spa-flower.png" id='flower1' />
+            <span className='macbeth'>TRAGEDY</span>
+            <span className='title-small'><img src="https://img.icons8.com/small/12/ffffff/spa-flower.png" id='flower1'/> OF</span>
+          </div>
+          <img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/20/ffffff/external-leaf-jungle-vitaliy-gorbachev-lineal-vitaly-gorbachev-1.png" className='leafImage'/>
+          <div className='titleName'>MAcBETH</div>
+          <div className='director'>Directed by Joel Coen</div>
+          <div id='tvPlusblue-links'>
+            <a href='https://tv.apple.com/gb/movie/umc.cmc.4wpfk1xmi22h3zyv4a10lj1tw?ign-itscg=10000&ign-itsct=atv-apl_hp-pmo_wch-Macbeth-220113' id='streamNow' target="_blank" rel="noreferrer">Stream now </a>
+            <span><img src="https://img.icons8.com/ios-filled/18/ffffff/circled-play.png" className='stream'/></span>
           </div>
         </div>
         <img src="https://www.apple.com/v/home/aj/images/promos/tv-plus-macbeth/promo_macbeth__e671onn69r8m_large.jpg" alt="tv+" className='tvPlusImg'></img>
